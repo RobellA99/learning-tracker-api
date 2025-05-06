b6237fbaa9f33aeca9a6b44d448464a475ea08cc
Robell <robellasfaw@gmail.com>
2025-05-06 16:49:26 +0100

    Correct api logic

--- controllers/skillsController.js
+++ controllers/skillsController.js
@@ -12,19 +12,29 @@ const getSkillsByCategory = async (req, res) => {
 
   try {
     const [results] = await connection.query(sql, [category_id]);
-    return res.status(200).json(results);
+
+    if (!results.length) {
+      return res.status(404).json({ message: "No skills found" });
+    }
+
+    res.json(results);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
 
-const getSkillsByResource = async (_req, res) => {
+const getSkillsByResource = async (req, res) => {
+  const { title } = req.query;
+
+  if (!title) {
+    return res.status(400).json({ message: "Resource is required" });
+  }
   const sql =
     "SELECT s.name, r.title AS resource_name FROM skills AS s JOIN resources AS r ON s.id = r.skill_id WHERE r.title = ?";
 
   try {
-    const [results] = await connection.query(sql);
+    const [results] = await connection.query(sql, [title]);
 
     if (!results.length) {
       return res.status(404).json({ message: "No skills found" });
