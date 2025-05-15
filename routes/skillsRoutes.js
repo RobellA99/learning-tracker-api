42007669507e8c6181d59b9b9b9785a810205a01
Robell <robellasfaw@gmail.com>
2025-05-15 22:16:02 +0100

    Add skill by category + resource routes

--- routes/skillsRoutes.js
+++ routes/skillsRoutes.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { addSkills, getSkills } from "../controllers/skillsController.js";
+import {
+  addSkills,
+  deleteSkill,
+  getSkills,
+  getSkillsByCategory,
+  getSkillsByResource,
+  updateSkill,
+} from "../controllers/skillsController.js";
 
 const router = express.Router();
 
-router.route("/").get(getSkills).post(addSkills);
+router
+  .route("/")
+  .get(getSkills)
+  .post(addSkills)
+  .delete(deleteSkill)
+  .patch(updateSkill);
+
+router.route("/category").get(getSkillsByCategory);
+
+router.route("/resource").get(getSkillsByResource);
 
 export default router;
