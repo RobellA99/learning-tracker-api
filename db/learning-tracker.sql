e373a5b0425404bfe1fca692eb17f7288ac1fdd9
Robell <robellasfaw@gmail.com>
2025-05-21 18:12:44 +0100

    Update password data type

--- db/learning-tracker.sql
+++ db/learning-tracker.sql
@@ -8,7 +8,7 @@ CREATE TABLE `users` (
   `id` SERIAL PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL UNIQUE,
-  `password` VARCHAR(255) NOT NULL,
+  `password` TEXT NOT NULL,
   `created_at` TIMESTAMP DEFAULT NOW(),
   `updated_at` TIMESTAMP DEFAULT NOW() ON UPDATE NOW()
 );
